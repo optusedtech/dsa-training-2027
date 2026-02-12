66c96786061b7108a59cc551357177cfc4ca1eac
optusedtech <edutech.optus@gmail.com>
2026-02-12 12:24:22 +0530

    template code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+const ll INF = 4e18;
+const int MOD = 1e9 + 7;
+
+void solve()
 {
-    int num;
-    cin >> num;
+    int n;
+    cin >> n;
     int rev = 0;
-    while (num != 0)
+    while (n != 0)
     {
-        int d = num % 10;
+        int d = n % 10;
         rev *= 10;
         rev += d;
-        num /= 10;
+        n /= 10;
     }
     cout << rev;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << endl;
+    }
+
+    return 0;
 }
